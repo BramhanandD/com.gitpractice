25994fa0e0667cfe7fcd88b9a7bed832831d6d45
pushpazhukeganhi <108128238+pushpazhukeganhi@users.noreply.github.com>
2024-05-04 16:22:14 +0530

    commit my changes to local git repository

--- src/test/java/com/gitpractice/testbase/DemoTest.java
+++ src/test/java/com/gitpractice/testbase/DemoTest.java
@@ -4,6 +4,14 @@ public class DemoTest {
 
 	public static void main(String[] args) {
 		System.out.println("Test");
+		
+		int age= 15;
+		if(age>=18) {
+		System.out.println("cast the vote");
+		}
+		else {
+			System.out.println("can not cast the vote");
+		}
 	}
 
 }
